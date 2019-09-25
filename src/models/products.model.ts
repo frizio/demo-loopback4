8c3847b90c1b5d103111e1f27b0015f808844ea2
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-09-25 22:46:28 +0200

    Create model Entity

---
+++ src/models/products.model.ts
@@ -0,0 +1,41 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {}})
+export class Products extends Entity {
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'number',
+    required: true,
+    default: 0,
+  })
+  price: number;
+
+  @property({
+    type: 'string',
+  })
+  description?: string;
+
+  @property({
+    type: 'number',
+    id: true,
+    required: true,
+    generated: true,
+  })
+  id: number;
+
+
+  constructor(data?: Partial<Products>) {
+    super(data);
+  }
+}
+
+export interface ProductsRelations {
+  // describe navigational properties here
+}
+
+export type ProductsWithRelations = Products & ProductsRelations;
